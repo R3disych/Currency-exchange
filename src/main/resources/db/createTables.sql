48bc35338dd2fb7066b39f10de0fc9f0057bccc8
R3disych <r3disych@gmail.com>
2025-03-30 17:01:19 +0300

    -create db

---
+++ src/main/resources/db/createTables.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS exchange_rates;
+DROP TABLE IF EXISTS currencies;
+
+CREATE TABLE currencies (
+    id SMALLSERIAL PRIMARY KEY NOT NULL,
+    code VARCHAR(3) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    sign VARCHAR(2) NOT NULL
+);
+
+CREATE TABLE exchange_rates (
+    id SERIAL PRIMARY KEY NOT NULL,
+    base_currency_id SMALLINT NOT NULL REFERENCES currencies (id),
+    target_currency_id SMALLINT NOT NULL REFERENCES currencies (id),
+    rate DECIMAL(6) NOT NULL,
+    UNIQUE (base_currency_id, target_currency_id)
+)
